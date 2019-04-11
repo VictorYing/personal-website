9fa5767b92616cf1a6030418c8afeaed4e65069f
Victor A. Ying <victory@csail.mit.edu>
2019-04-10 22:06:58 -0400

    [publish] Add script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+if [ -n "$(git status --porcelain --ignore-submodules)" ]; then
+  echo "Git status isn't clean!"
+  exit
+fi
+
+GIT_VERSION="$(git rev-parse HEAD)"
+
+echo "Generating and publishing based on commit $GIT_VERSION"
+
+hugo
+cd public
+git add .
+git commit -m "Generated from commit $GIT_VERSION"
+#git push
