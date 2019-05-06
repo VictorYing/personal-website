9eec41bf6666ae5203bfe29cf6e4f5060e2bc5b0
Victor A. Ying <victory@csail.mit.edu>
2019-05-06 14:24:53 -0400

    [view.sh] Minor

--- view.sh
+++ view.sh
@@ -4,7 +4,7 @@ HOSTNAME="$(hostname -A | cut -f 1 -d ' ')"
 ADDRESS="$(hostname -I | cut -f 1 -d ' ')"
 PORT="13131"
 
-echo "Launching server at $HOSTNAME ($ADDRESS:$PORT)"
+echo "Launching server on port $PORT at $HOSTNAME ($ADDRESS)"
 
 hugo server \
   --baseURL "http://$HOSTNAME:$PORT/" \
